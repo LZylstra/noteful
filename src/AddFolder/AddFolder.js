4e61213c56909bedc1a1f49620e181e4d0d0cb71
Lindsey Zylstra <lindseyzylstra@gmail.com>
2019-11-30 11:12:14 -0800

    updated AddFolder

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 class AddFolder extends React.Component {
   static defaultProps ={
-    onAddNote: () => {},
+    onAddFolder: () => {},
   }
 
   static contextType = ApiContext;
