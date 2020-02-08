cddf900485f9f6b9ca4621aec67f867fea48b0ac
LZylstra <lindseyzylstra@gmail.com>
2020-02-08 15:57:46 -0800

    fixed delete button in note view

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -1,39 +1,60 @@
-import React from 'react'
-import Note from '../Note/Note'
-import ApiContext from '../ApiContext'
-import { findNote } from '../notes-helpers'
-import './NotePageMain.css'
+import React from "react";
+import Note from "../Note/Note";
+import ApiContext from "../ApiContext";
+import { findNote } from "../notes-helpers";
+import "./NotePageMain.css";
+import config from "../config";
 
 export default class NotePageMain extends React.Component {
   static defaultProps = {
     match: {
       params: {}
     }
-  }
-  static contextType = ApiContext
+  };
+  static contextType = ApiContext;
 
-  handleDeleteNote = noteId => {
-    this.props.history.push(`/`)
-  }
+  handleClickDelete = e => {
+    e.preventDefault();
+    const noteId = this.props.id;
+
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) return res.json().then(e => Promise.reject(e));
+        return res.json();
+      })
+      .then(() => {
+        this.context.deleteNote(noteId);
+        // allow parent to perform extra behaviour
+        this.props.onDeleteNote(noteId);
+      })
+      .catch(error => {
+        console.error({ error });
+      });
+  };
 
   render() {
-    const { notes=[] } = this.context
-    const { noteId } = this.props.match.params
-    const note = findNote(notes, noteId) || { content: '' }
+    const { notes = [] } = this.context;
+    const { noteId } = this.props.match.params;
+    const note = findNote(notes, noteId) || { content: "" };
     return (
-      <section className='NotePageMain'>
+      <section className="NotePageMain">
         <Note
           id={note.id}
           name={note.name}
           modified={note.modified}
-          onDeleteNote={this.handleDeleteNote}
+          onDeleteNote={this.handleClickDelete}
         />
-        <div className='NotePageMain__content'>
-          {note.content.split(/\n \r|\n/).map((para, i) =>
+        <div className="NotePageMain__content">
+          {note.content.split(/\n \r|\n/).map((para, i) => (
             <p key={i}>{para}</p>
-          )}
+          ))}
         </div>
       </section>
-    )
+    );
   }
 }
